61513eee8c1df1abd3380de697c765cee5ff9f1b
Fred F <perso.dsgjohnson@outlook.fr>
2025-04-15 16:12:49 +0200

    changed the base state of the vote

--- src/store/use-isvote-waiting-code.ts
+++ src/store/use-isvote-waiting-code.ts
@@ -1,7 +1,7 @@
 import { atomWithStorage } from 'jotai/utils'
 import { useAtom } from 'jotai'
 
-const modalAtom = atomWithStorage<boolean>('is-vote-waiting-code', true)
+const modalAtom = atomWithStorage<boolean>('is-vote-waiting-code', false)
 const emailAtom = atomWithStorage<string | null>('vote-request-email', null)
 
 export const useIsVoteWaitingCode = () => {
